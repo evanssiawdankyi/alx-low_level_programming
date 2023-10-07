8dda2a8989a8dbe722a7da9be1784756ee6a6da2
evanssiawdankyi <evanssiawdankyi@yahoo.com>
2023-10-06 22:44:08 -0700

    updated

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,10 +3,10 @@
 #include <string.h>
 
 /**
-* main - Prints the alphabet without q and e.
-*
-* Return: Always 0 (Success)
-*/
+ * main - Prints the alphabet without q and e.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-* main - prints the lowercase alphabet in reverse,
-* followed by a new line
-* Return: Always 0 (Success)
-*/
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char ch;
